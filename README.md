c399ccdb8e3c268c6cf9028fbe790ec8917f9482
nolood <a9514548799@gmail.com>
2023-11-10 00:20:37 -0800

    feat: init config

---
+++ README.md
@@ -0,0 +1,4 @@
+## Для установки переменной окружения и загрузки конфига
+
+```sh
+$env:CONFIG_PATH="./config/local.yaml"; go run .\cmd\rest-api\main.go```
--- cmd/rest-api/main.go
+++ cmd/rest-api/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"time-manage-go/internal/config"
+)
+
 func main() {
 	//	TODO: init config
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
 
 	//	TODO: init logger
 
